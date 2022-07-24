ced3714772114e7126ad30cfafd1ba00ddc0921c
manuel1710 <javian99anderson@gmail.com>
2022-07-24 04:25:22 -0500

    Proper Configurations

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,8 +1,5 @@
 import React, { useState } from "react";
 import "../App.css";
-import { storage } from "../firebase.js";
-import { ref, uploadBytes } from "firebase/storage";
-import { v4 } from "uuid";
 import Navbar from "../components/Navbar";
 import axios from "axios";
 
@@ -11,33 +8,21 @@ axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 function Home() {
   const [imageUpload, setImageUpload] = useState(null);
   const [folderName, setFolderName] = useState(null);
-  // const [data, setData] =useState({
-  //   name: '',
-  //   folder: '',
-  //   file: null
-  // })
 
   const uploadImage = async (event) => {
     event.preventDefault();
     const form = document.querySelector("form");
     const formData = new FormData(form);
 
-    let file = null;
     if (imageUpload == null) {
       alert("No Image Selected");
       return;
-    }
-    else if (imageUpload !== null) {
-       file = event.target.file.files[0];
     }
     if (folderName == null) {
       alert("No Folder Selected");
       return;
     }
     
-
-    let name = document.querySelector('#name').value;
-    let folder = document.querySelector('#folder').value;
     
 
     const url = "http://localhost:5000/file/upload";
@@ -58,26 +43,14 @@ function Home() {
 
 
 
-    // const imageRef = ref(storage, `${folderName}/${Date()}`);
-    // uploadBytes(imageRef, imageUpload).then(() => {
-    //   alert("Image Uploaded");
-    //   //   window.location.reload(false);
-    //   window.location.href = "/gallery";
-    // });
-
 
   };
-
-  function handle(event) {
-  
-  }
 
   return (
     <div>
       <Navbar />
       <div className="container">
         <div className="m-auto">
-          {/* <form action="/upload" method="POST" encType="multipart/form-data" onSubmit={(event) => uploadImage(event)}> */}
           <form encType="multipart/form-data" onSubmit={(event) => uploadImage(event)}>
             <div className="row text-center">
               <div className="mb-3">
@@ -92,7 +65,6 @@ function Home() {
                 />
                 <label htmlFor="file" className="btn-danger m-3">Choose File to Upload</label>
               </div>
-              {/* <div className="">{imageUpload[]}</div> */}
 
               <input type="text" placeholder="Name your File" id="name" name="name"/>
 
@@ -118,7 +90,6 @@ function Home() {
                 </select>
               </label>
               <div className="p-5">
-                {/* <button className="w-75 btn-primary" onClick={uploadImage}> */}
                 <button className="w-75 btn-primary">
                   Upload Image
                 </button>
