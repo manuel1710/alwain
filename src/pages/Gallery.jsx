46eb12c24d9db6ce7f51ac095e8ec242f09b0194
manuel1710 <javian99anderson@gmail.com>
2022-07-24 21:37:55 -0500

    new commit

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,20 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "../App.css";
-import { storage } from "../firebase.js";
-import {
-  ref,
-  uploadBytes,
-  listAll,
-  getDownloadURL,
-  list,
-} from "firebase/storage";
-import { v4 } from "uuid";
-import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import axios from "axios";
+import {Buffer} from 'buffer';
 // import { SRLWrapper } from "simple-react-lightbox";
 
 function Gallery() {
+  const url = 'http://localhost:5000/file/upload';
+  // const url = "https://alwainserver.herokuapp.com/file/upload";
   const [imageList, setImageList] = useState([]);
   const [imageName, setImageName] = useState([]);
   // const [itemName, setItemName] = useState([]);
@@ -22,30 +15,26 @@ function Gallery() {
   var deleteName = '';
   var deleteDate ='';
   var deleteFileName = '';
-  var imageListRef = ref(storage, "people/");
   const folders = ["school", "work", "hospital", "laboratory", "freelance"];
 
   const [data, setData] = useState([]);
 
   function getAll() {
-    // console.log(itemName);
     axios
-      .get("http://localhost:5000/file/upload",{params:{tag: ''}})
+      .get(url,{params:{tag: ''}})
       .then((res) => setData(res.data))
       .catch((err) => console.log(err, "it has an error"));
   }
 
   function getFolder() {
-    // console.log(itemName);
     axios
-      .get("http://localhost:5000/file/upload", {params:{tag: itemName}})
+      .get(url, {params:{tag: itemName}})
       .then((res) => setData(res.data))
       .catch((err) => console.log(err, "it has an error"));
   }
   function deleteFile() {
-    // console.log(itemName);
     axios
-      .delete("http://localhost:5000/file/upload", {params:{name: deleteName, uploadDate:deleteDate, filename:deleteFileName}})
+      .delete(url, {params:{name: deleteName, uploadDate:deleteDate, filename:deleteFileName}})
       .then((res) => setData(res.data))
       .catch((err) => console.log(err, "it has an error"));
       
@@ -73,9 +62,7 @@ function Gallery() {
               <li key={i} className="col p-3">
                 <button
                   className="text-uppercase btn-success"
-                  // onClick={getFolder(item)}
                   onClick={()=>{
-                    // setItemName(item);
                     itemName = item;
                     getFolder();
 
